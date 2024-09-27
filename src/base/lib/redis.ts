01e0eec6e08bb35d3469e596d078c33942cd4c31
Vedant Mhapsekar <72020074+ee3devYT@users.noreply.github.com>
2024-09-27 19:45:58 +0530

    Update redis.ts

--- src/base/lib/redis.ts
+++ src/base/lib/redis.ts
@@ -2,7 +2,7 @@ import consola from 'consola';
 import Redis, { RedisOptions } from 'ioredis';
 
 function getRedisUri(): string {
-    const redisUri: string | undefined = process.env.REDIS_URI || "redis://default:xYy724C5jPTrb6419dZYgQpEfzY8w6t8@redis-11592.c114.us-east-1-4.ec2.redns.redis-cloud.com:11592";
+    const redisUri: string | undefined = process.env.REDIS_URI || "";
     
     if (!redisUri) {
         consola.error('REDIS_URI is not defined in the environment variables');
